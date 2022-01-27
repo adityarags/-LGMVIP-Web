bfe661a20eabfa05a325330d1ca2ff367342643f
adityarags <adityarags@gmail.com>
2022-01-28 01:52:07 +0530

    Task 1 Final

--- Task 1/app.js
+++ Task 1/app.js
@@ -1,9 +1,13 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+document.getElementById("discountbox").addEventListener("click", function() {
+    window.confirm("Use the Discount Code: REGULAR50");
 });
+
+
+document.getElementById("discountbox1").addEventListener("click", function() {
+    window.confirm("Use the Discount Code: NEW20");
+});
+
+document.getElementById("discountbox2").addEventListener("click", function() {
+    window.confirm("Use the Discount Code: ALL15");
+});
+
